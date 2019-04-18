541563c93151a660d4032d8fe6b4e1fef9f3bc62
caoimhinr <kevinwelvaert@gmail.com>
2019-04-18 14:01:45 +0200

    commit

--- Assets/Menu_Buttons.cs
+++ Assets/Menu_Buttons.cs
@@ -6,27 +6,31 @@ public class Menu_Buttons : MonoBehaviour
 {
     public GameObject MenuPanel;
     public GameObject ShopPanel;
+    public GameObject BattlePanel;
     // Start is called before the first frame update
     public void Start()
     {
         MenuPanel.SetActive(true);
         ShopPanel.SetActive(false);
+        BattlePanel.SetActive(false);
     }
 
     // Update is called once per frame
     public void Update()
     {
-        
+
     }
 
     public void ShowMenuPanel()
     {
         MenuPanel.SetActive(true);
         ShopPanel.SetActive(false);
+        BattlePanel.SetActive(false);
     }
     public void ShowShopPanel()
     {
         MenuPanel.SetActive(false);
         ShopPanel.SetActive(true);
+        BattlePanel.SetActive(false);
     }
 }
